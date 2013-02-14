c52c1626968722ef07032785406993defbdb3ac3
0ip <me@factor.cc>
2013-02-14 19:58:22 +0100

    Update install-deps.sh

--- install-deps.sh
+++ install-deps.sh
@@ -1,13 +1,16 @@
+sudo apt-get update
+
 # https://github.com/rupa/z
 mkdir -p ~/code/z
 curl https://raw.github.com/rupa/z/master/z.sh > ~/code/z/z.sh
 chmod +x ~/code/z/z.sh
 
-
 sudo apt-get install python-pip
 sudo easy_install Pygments
-
 
 sudo apt-get install libwww-perl
 
 mkdir -p ~/.vim/backups ~/.vim/swaps ~/.vim/undo
+
+# Ubuntu-specific
+sudo apt-get remove unity-lens-shopping command-not-found

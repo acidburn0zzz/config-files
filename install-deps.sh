ca53a5ec32ee62ffe6c72452711f668759699e1d
0ip <me@factor.cc>
2013-04-19 18:17:35 +0200

    Remove z-command

--- install-deps.sh
+++ install-deps.sh
@@ -1,9 +1,5 @@
 sudo apt-get update
-
-# https://github.com/rupa/z
-mkdir -p ~/code/z
-curl https://raw.github.com/rupa/z/master/z.sh > ~/code/z/z.sh
-chmod +x ~/code/z/z.sh
+sudo apt-get install curl tmux vim
 
 sudo apt-get install python-pip
 sudo easy_install Pygments

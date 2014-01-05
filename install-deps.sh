de40948acb64422f5f5d0613b60b5e69978868ee
0ip <me@factor.cc>
2014-01-05 13:27:37 +0100

    Remove powerline-shell

--- install-deps.sh
+++ install-deps.sh
@@ -1,15 +1,8 @@
 sudo apt-get update
 sudo apt-get purge vim*
-sudo apt-get install curl tmux vim-nox python-pygments exuberant-ctags libwww-perl cmake
+sudo apt-get install git curl tmux vim-nox python-pygments exuberant-ctags libwww-perl cmake
 
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-
-git clone https://github.com/milkbikis/powerline-shell
-cd powerline-shell
-mv ../config.py config.py
-./install.py
-cd ..
-ln -sf `pwd`/powerline-shell/powerline-shell.py ~/powerline-shell.py
 
 find . -type f -name ".*" | xargs -i ln -sf `pwd`/{} ~/\{}
 
